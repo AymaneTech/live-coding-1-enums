663693314c79cf7128c28c584cad9c88ecccbb9b
geek <elmainiaymane03@gmail.com>
2024-09-03 02:54:18 +0100

    finish the innerclasses

---
+++ src/main/java/com/wora/VehicleType.java
@@ -0,0 +1,89 @@
+package com.wora;
+
+public enum VehicleType implements Drivable, Identifiable, Insurable {
+
+    CAR(4, "Gasoline") {
+        @Override
+        public void insure() {
+            System.out.println("Car insurance is being processed");
+        }
+
+        @Override
+        public void drive() {
+            System.out.println("Driving a car with " + this.getFuelType() + " fuel.");
+        }
+    },
+
+    MOTORCYCLE(2, "Gasoline") {
+        @Override
+        public void insure() {
+            System.out.println("Motorcycle car insurance is being processed.");
+        }
+
+        @Override
+        public void drive() {
+            System.out.println("Riding a motorcycle with " + this.getFuelType() + " fuel.");
+        }
+    },
+
+    ELECTRIC_CAR(4, "Electric") {
+        @Override
+        public void insure() {
+            System.out.println("Electric car insurance is being processed.");
+        }
+
+        @Override
+        public void drive() {
+            System.out.println("Driving an electric car with " + this.getFuelType() + " fuel.");
+        }
+    };
+
+    private final Integer numberOfWheels;
+    private final String fuelType;
+
+    VehicleType(Integer numberOfWheels, String fuelType) {
+        this.numberOfWheels = numberOfWheels;
+        this.fuelType = fuelType;
+    }
+
+    public Integer getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public Engine getEngine(Integer horsepower) {
+        return new Engine(horsepower);
+    }
+
+    @Override
+    public String getIdentifier() {
+        return this.name() + "@" + this.ordinal();
+    }
+
+    public class Engine {
+        private final Integer horsepower;
+
+        public Engine(Integer horsepower) {
+            this.horsepower = horsepower;
+        }
+
+        public void start() {
+            System.out.println("Engine with " + horsepower + " HP started for " + VehicleType.this.name());
+        }
+    }
+}
+
+interface Identifiable {
+    String getIdentifier();
+}
+
+interface Insurable {
+    void insure();
+}
+
+interface Drivable {
+    void drive();
+}
